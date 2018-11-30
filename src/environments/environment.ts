c98f599f2548d14126a618753ab1e042ede5a308
Ivan Cardozo <34973566+ivancardozo11@users.noreply.github.com>
2018-11-30 03:08:15 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
    firebase : {
-   apiKey: "AIzaSyC7nMcqFlDYrb_KHVi8BnvuGXvlGMih_7Q",
-   authDomain: "tripiou-e36a5.firebaseapp.com",
-   databaseURL: "https://tripiou-e36a5.firebaseio.com",
-   projectId: "tripiou-e36a5",
-   storageBucket: "tripiou-e36a5.appspot.com",
-   messagingSenderId: "986783413729"
+   
  }
 };
 
